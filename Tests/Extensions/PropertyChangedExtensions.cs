4fde8ed98ddd53da867ca1a68ca6814473b2a2ec
Oliver Brown <galaxiaguy@gmail.com>
2017-08-24 21:06:02 +0100

    Rename and move extension tests.

--- Tests/Extensions/PropertyChangedExtensions.cs
+++ Tests/Extensions/PropertyChangedExtensions.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using Xunit;
 
-namespace GamesWithGravitas
+namespace GamesWithGravitas.Extensions
 {
 	public class PropertyChangedExtensionsTests
 	{
