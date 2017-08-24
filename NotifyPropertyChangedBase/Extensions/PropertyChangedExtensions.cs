a8ba6d4bf193ae7122c81a328f8d3bc084ad7bf7
Oliver Brown <galaxiaguy@gmail.com>
2017-08-24 19:26:05 +0100

    Add  core files.

---
+++ NotifyPropertyChangedBase/Extensions/PropertyChangedExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesWithGravitas.Extensions
+{
+    public static class PropertyChangedExtensions
+    {
+        public static PropertyChangedListener ListenForPropertyChanged(this INotifyPropertyChanged target, params string[] properties)
+        {
+            var listener = new PropertyChangedListener(properties);
+            listener.Subscribe(target);
+            return listener;
+        }
+
+        public class PropertyChangedListener : IDisposable
+        {
+            public PropertyChangedListener(IEnumerable<string> properties)
+            {
+                Results = properties.ToDictionary(x => x, x => false);
+            }
+
+            private List<INotifyPropertyChanged> _targets = new List<INotifyPropertyChanged>();
+
+            public Dictionary<string, bool> Results;
+
+            public IEnumerable<string> Successes => Results.Where(x => x.Value).Select(x => x.Key);
+
+            public bool AllTrue => Results.Values.All(x => x);
+
+            public string FailMessage => string.Join(" ", Results.Where(x => !x.Value).Select(x => string.Format("PropertyChanged not called for {{{0}}}.", x.Key)));
+
+            public void Subscribe(INotifyPropertyChanged target)
+            {
+                target.PropertyChanged += TargetPropertyChanged;
+                _targets.Add(target);
+            }
+
+            public void Unsubscribe(INotifyPropertyChanged target)
+            {
+                target.PropertyChanged -= TargetPropertyChanged;
+                _targets.Remove(target);
+            }
+
+            private void TargetPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (Results.ContainsKey(e.PropertyName))
+                {
+                    Results[e.PropertyName] = true;
+                }
+            }
+
+            public void Dispose()
+            {
+                foreach (var target in _targets.ToList())
+                {
+                    Unsubscribe(target);
+                }
+            }
+        }
+    }
+}
---
+++ NotifyPropertyChangedBase/NotifyPropertyChangedBase.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GamesWithGravitas
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(params string[] propertyNames)
+        {
+            if (PropertyChanged == null)
+            {
+                return;
+            }
+            foreach (var propertyName in propertyNames)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string whichProperty = null, params string[] otherProperties) where T : class
+        {
+            if (field == null && value == null)
+            {
+                return false;
+            }
+            else if (field != null && field.Equals(value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(whichProperty);
+            OnPropertyChanged(otherProperties);
+            return true;
+        }
+
+        protected bool SetEnumProperty<T>(ref T field, T value, [CallerMemberName] string whichProperty = null, params string[] otherProperties)
+        {
+            if (field.Equals(value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(whichProperty);
+            OnPropertyChanged(otherProperties);
+            return true;
+        }
+
+        protected bool SetProperty(ref int field, int value, [CallerMemberName] string whichProperty = null, params string[] otherProperties)
+        {
+            if (field == value)
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(whichProperty);
+            OnPropertyChanged(otherProperties);
+            return true;
+        }
+
+        protected bool SetProperty(ref bool field, bool value, [CallerMemberName] string whichProperty = null, params string[] otherProperties)
+        {
+            if (field == value)
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(whichProperty);
+            OnPropertyChanged(otherProperties);
+            return true;
+        }
+
+        protected bool SetProperty(ref decimal field, decimal value, [CallerMemberName] string whichProperty = null, params string[] otherProperties)
+        {
+            if (field == value)
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(whichProperty);
+            OnPropertyChanged(otherProperties);
+            return true;
+        }
+    }
+}
