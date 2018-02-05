f8bdfaaf89be4a3c1a3ab8938f06ff5d2bab0020
Oliver Brown <galaxiaguy@gmail.com>
2018-02-05 22:15:25 +0000

    Fix skia page XAML.

--- XamarinForms.Sample/XamarinForms.Sample/SkiaPage.xaml.cs
+++ XamarinForms.Sample/XamarinForms.Sample/SkiaPage.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace GamesWithGravitas.XamarinForms.Sample
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkiaPage : ContentPage
 	{
 		public SkiaPage()
@@ -18,7 +11,7 @@ namespace GamesWithGravitas.XamarinForms.Sample
             animatedView.AnimateAsync();
 		}
 
-        private void ButtonClicked(object sender, EventHandler e)
+        private void ButtonClicked(object sender, EventArgs e)
         {
             animatedView.AnimateAsync();
         }
