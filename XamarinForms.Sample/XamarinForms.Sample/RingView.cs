37c89c44bdb4a1f1220d61f545b72bb8950e4d41
Oliver Brown <galaxiaguy@gmail.com>
2018-02-05 22:30:34 +0000

    Update sample project.

---
+++ XamarinForms.Sample/XamarinForms.Sample/RingView.cs
@@ -0,0 +1,26 @@
+﻿using GamesWithGravitas.XamarinForms.SkiaSharp;
+using System;
+using SkiaSharp;
+using Xamarin.Forms;
+using SkiaSharp.Views.Forms;
+
+namespace GamesWithGravitas.XamarinForms.Sample
+{
+    public class RingView : SKCanvasChildView
+    {
+        public RingView()
+        {
+        }
+
+        public override void Paint(SKSurface surface, SKImageInfo info)
+        {
+            var canvas = surface.Canvas;
+            var width = canvas.LocalClipBounds.Width;
+            var height = canvas.LocalClipBounds.Height;
+
+            var paint = new SKPaint { Color = Color.Black.ToSKColor(), IsStroke = true, StrokeWidth=0.075f*width };
+            canvas.Clear(Color.Blue.ToSKColor());
+            canvas.DrawCircle(width / 2, height / 2, width / 3, paint);
+        }
+    }
+}
