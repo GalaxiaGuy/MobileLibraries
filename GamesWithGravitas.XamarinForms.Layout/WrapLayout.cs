66276310c16d96d601ce96006365157243462753
Oliver Brown <oliver.brown@chelsea-apps.com>
2018-05-31 15:41:01 +0100

    Add support for distributing items in a wrap layout.

--- GamesWithGravitas.XamarinForms.Layout/WrapLayout.cs
+++ GamesWithGravitas.XamarinForms.Layout/WrapLayout.cs
@@ -13,8 +13,8 @@ namespace GamesWithGravitas.XamarinForms.Layout
         public static readonly BindableProperty OrientationProperty =
             BindableProperty.Create(nameof(Orientation), typeof(StackOrientation), typeof(WrapLayout), StackOrientation.Horizontal, propertyChanged: (bindable, oldValue, newValue) => ((IWrapElement)bindable).InvalidateLayout());
 
-        public static readonly BindableProperty TryDistributeProperty =
-            BindableProperty.Create(nameof(TryDistribute), typeof(bool), typeof(WrapLayout), false, propertyChanged: (bindable, oldValue, newValue) => ((IWrapElement)bindable).InvalidateLayout());
+        public static readonly BindableProperty DistributeProperty =
+            BindableProperty.Create(nameof(Distribute), typeof(bool), typeof(WrapLayout), false, propertyChanged: (bindable, oldValue, newValue) => ((IWrapElement)bindable).InvalidateLayout());
 
         public double ColumnSpacing
         {
@@ -34,10 +34,10 @@ namespace GamesWithGravitas.XamarinForms.Layout
             set => SetValue(OrientationProperty, value);
         }
 
-        public bool TryDistribute
+        public bool Distribute
         {
-            get => (bool)GetValue(TryDistributeProperty);
-            set => SetValue(TryDistributeProperty, value);
+            get => (bool)GetValue(DistributeProperty);
+            set => SetValue(DistributeProperty, value);
         }
 
         void IWrapElement.InvalidateLayout() => InvalidateLayout();
@@ -114,7 +114,7 @@ namespace GamesWithGravitas.XamarinForms.Layout
                 {
                     if (mainAxisLength + ColumnSpacing + sizeRequest.Request.Width > width)
                     {
-                        crossAxisItems.Add(new CrossAxisItem { Children = crossAxisChildren, X = x, Y = y, MainAxisLength = width, CrossAxisLength = crossAxisLength });
+                        crossAxisItems.Add(new CrossAxisItem(Orientation, ColumnSpacing, crossAxisChildren, x, y, crossAxisLength, width));
                         crossAxisChildren = new List<SizedView>();
                         y += crossAxisLength + RowSpacing;
                         mainAxisLength = sizeRequest.Request.Width;
@@ -132,7 +132,7 @@ namespace GamesWithGravitas.XamarinForms.Layout
                 {
                     if (mainAxisLength + RowSpacing + sizeRequest.Request.Height > height)
                     {
-                        crossAxisItems.Add(new CrossAxisItem { Children = crossAxisChildren, X = x, Y = y, MainAxisLength = height, CrossAxisLength = crossAxisLength });
+                        crossAxisItems.Add(new CrossAxisItem(Orientation, ColumnSpacing, crossAxisChildren, x, y, crossAxisLength, height));
                         crossAxisChildren = new List<SizedView>();
                         x += crossAxisLength + ColumnSpacing;
                         mainAxisLength = sizeRequest.Request.Height;
@@ -149,7 +149,11 @@ namespace GamesWithGravitas.XamarinForms.Layout
             }
             if (Orientation == StackOrientation.Horizontal)
             {
-                crossAxisItems.Add(new CrossAxisItem { Children = crossAxisChildren, X = x, Y = y, MainAxisLength = width, CrossAxisLength = crossAxisLength });
+                crossAxisItems.Add(new CrossAxisItem(Orientation, ColumnSpacing, crossAxisChildren, x, y, crossAxisLength, width));
+                if (Distribute)
+                {
+                    crossAxisItems.Distribute();
+                }
                 foreach (var item in crossAxisItems)
                 {
                     LayoutRow(item);
@@ -157,7 +161,11 @@ namespace GamesWithGravitas.XamarinForms.Layout
             }
             else
             {
-                crossAxisItems.Add(new CrossAxisItem { Children = crossAxisChildren, X = x, Y = y, MainAxisLength = height, CrossAxisLength = crossAxisLength });
+                crossAxisItems.Add(new CrossAxisItem(Orientation, RowSpacing, crossAxisChildren, x, y, crossAxisLength, height));
+                if (Distribute)
+                {
+                    crossAxisItems.Distribute();
+                }
                 foreach (var item in crossAxisItems)
                 {
                     LayoutColumn(item);
@@ -165,48 +173,9 @@ namespace GamesWithGravitas.XamarinForms.Layout
             }
         }
 
-        /// <summary>
-        /// A row when vertical, a column when horizontal
-        /// </summary>
-        internal class CrossAxisItem
-        {
-            public List<SizedView> Children;
-            public double X;
-            public double Y;
-            public double MainAxisLength;
-            public double CrossAxisLength;
-
-            public void Deconstruct(out List<SizedView> children, out double x, out double y, out double mainAxisLength, out double crossAxisLength)
-            {
-                children = Children;
-                x = X;
-                y = Y;
-                mainAxisLength = MainAxisLength;
-                crossAxisLength = CrossAxisLength;
-            }
-        }
-
-        internal class SizedView
-        {
-            public View View;
-            public Size Size;
-
-            public SizedView(View view, Size size)
-            {
-                View = view;
-                Size = size;
-            }
-
-            public void Deconstruct(out View view, out Size size)
-            {
-                view = View;
-                size = Size;
-            }
-        }
-
         private void LayoutRow(CrossAxisItem item)
         {
-            var (children, x, y, width, height) = item;
+            var (children, x, y, _, height, width) = item;
 
             var minimumWidth = children.Sum(c => c.Size.Width);
             var extraAvailableWidth = width - minimumWidth;
@@ -234,7 +203,7 @@ namespace GamesWithGravitas.XamarinForms.Layout
 
         private void LayoutColumn(CrossAxisItem item)
         {
-            var (children, x, y, height, width) = item;
+            var (children, x, y, _, width, height) = item;
 
             var minimumHeight = children.Sum(c => c.Size.Height);
             var extraAvailableHeight = height - minimumHeight;
@@ -242,7 +211,7 @@ namespace GamesWithGravitas.XamarinForms.Layout
             var expandingChlidCount = expandingChildren.Count();
             var expandingChildHeight = expandingChildren.Sum(c => c.Size.Height);
 
-            var spareHeight = width - minimumHeight - (children.Count - 1) * RowSpacing;
+            var spareHeight = height - minimumHeight - (children.Count - 1) * RowSpacing;
             foreach ((var child, var size) in children)
             {
                 if (child.VerticalOptions.Expands)
@@ -256,6 +225,108 @@ namespace GamesWithGravitas.XamarinForms.Layout
                 {
                     LayoutChildIntoBoundingRegion(child, new Rectangle(x, y, width, size.Height));
                     y += size.Height + RowSpacing;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// A row when vertical, a column when horizontal
+    /// </summary>
+    internal class CrossAxisItem
+    {
+        public StackOrientation Orientation { get; }
+        public double Spacing { get; }
+        public List<SizedView> Children { get; }
+        public double X { get; }
+        public double Y { get; }
+        public double MaximumMainAxisLength { get; }
+        public double CrossAxisLength { get; }
+        public double TotalSpacing => Children?.Count > 0 ? (Children.Count - 1) * Spacing : 0;
+        public double MainAxisLength => Children?.Count > 0 ? Children.Sum(x => x.MainAxisLength(Orientation)) + TotalSpacing : 0;
+
+        public CrossAxisItem(StackOrientation orientation, double spacing, List<SizedView> chlidren, double x, double y, double crossAxisLength, double maximumMainAxisLength)
+        {
+            Orientation = orientation;
+            Spacing = spacing;
+            Children = chlidren;
+            X = x;
+            Y = y;
+            CrossAxisLength = crossAxisLength;
+            MaximumMainAxisLength = maximumMainAxisLength;
+        }
+
+        public void Deconstruct(out List<SizedView> children, out double x, out double y, out double mainAxisLength, out double crossAxisLength, out double maximumMainAxisLength)
+        {
+            children = Children;
+            x = X;
+            y = Y;
+            crossAxisLength = CrossAxisLength;
+            mainAxisLength = MainAxisLength;
+            maximumMainAxisLength = MaximumMainAxisLength;
+        }
+    }
+
+    internal class SizedView
+    {
+        public View View;
+        public Size Size;
+
+        public SizedView(View view, Size size)
+        {
+            View = view;
+            Size = size;
+        }
+
+        public void Deconstruct(out View view, out Size size)
+        {
+            view = View;
+            size = Size;
+        }
+
+        public double MainAxisLength(StackOrientation orientation) => orientation == StackOrientation.Horizontal ? Size.Width : Size.Height;
+    }
+
+    internal static class CrossAxisItemExtensions
+    {
+        public static void Distribute(this IList<CrossAxisItem> items)
+        {
+            if (items?.Any() != true)
+            {
+                return;
+            }
+            var orientation = items[0].Orientation;
+            var spacing = items[0].Spacing;
+            for (int i = 0; i < items.Count - 1; i++)
+            {
+                var thisItem = items[i];
+                var nextItem = items[i + 1];
+
+                while (true)
+                {
+                    var candidate = thisItem.Children?.LastOrDefault();
+                    if (candidate == null)
+                    {
+                        break;
+                    }
+
+                    var thisItemCurrentLength = thisItem.MainAxisLength;
+                    var nextItemCurrentLength = nextItem.MainAxisLength;
+                    var currentDiff = Math.Abs(thisItemCurrentLength - nextItemCurrentLength);
+
+                    var thisItemCandidateLength = thisItemCurrentLength - candidate.MainAxisLength(orientation) - spacing;
+                    var nextItemCandidateLength = nextItemCurrentLength + candidate.MainAxisLength(orientation) - spacing;
+                    var candidateDiff = Math.Abs(thisItemCandidateLength - nextItemCandidateLength);
+
+                    if (candidateDiff < currentDiff)
+                    {
+                        thisItem.Children.Remove(candidate);
+                        nextItem.Children.Insert(0, candidate);
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
         }
